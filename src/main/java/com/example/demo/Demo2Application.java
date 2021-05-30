835810757f1a8ad8ce86bf8a49e42a02aac5adf6
Paramesh Reddy <parameshwer98@gmail.com>
2021-05-30 17:40:25 +0530

    Starting commited via intellij ide

---
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Demo2Application {
+
+	@ResponseBody
+	@RequestMapping({"/","/hello"})
+	public String hello(){
+		return "Hello Parameshwar Redddy";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+}
